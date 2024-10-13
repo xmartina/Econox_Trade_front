a81bd40d4f8918d2b960685b732f19b8ffe250a7
xmartina <martinsprince126@gmail.com>
2024-10-13 11:06:12 +0100

    d

--- include/config.php
+++ include/config.php
@@ -2,8 +2,8 @@
 $site_name = 'EconoXtrade Investment';
 $site_url = 'https://econoxtrade.online/';
 $base_link = 'econoxtrade.online';
-$login_url = 'account.'.$base_link.'/?a=login';
-$signup_url = 'account.'.$base_link.'/?a=signup';
+$login_url = 'https://account.'.$base_link.'/?a=login';
+$signup_url = 'https://account.'.$base_link.'/?a=signup';
 $site_logo = $site_url . 'assets/images/logoIcon/logo.png';
 $site_favicon = $site_url . 'assets/images/logoIcon/favicon.png';
 
