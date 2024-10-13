8a3ae48b81f568c962bf840e472f501b56813c73
xmartina <martinsprince126@gmail.com>
2024-10-13 13:28:53 +0100

    d

--- pages/plans/index.php
+++ pages/plans/index.php
@@ -1,1 +1,12 @@
 <?php
+$page_name = 'Our Plans';
+include_once(__DIR__ . '/../../functions/home_function.php');
+include_once(__DIR__ . '/../../partials/header.php');
+include_once(__DIR__ . '/../../partials/breadcrumb.php');
+?>
+
+
+
+<?php
+include_once(__DIR__ . '/../../partials/footer.php');
+?>
