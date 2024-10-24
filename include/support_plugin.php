883c41fc66fc352ea846d4d534dcb3f012974afd
xmartina <martinsprince126@gmail.com>
2024-10-24 14:19:57 +0100

    d

--- include/support_plugin.php
+++ include/support_plugin.php
@@ -3,15 +3,21 @@
 <div class="elfsight-app-f1dca61f-fa48-4ce1-9417-512d2c1d8617" data-elfsight-app-lazy></div>
 <script>
     window.addEventListener('load', function() {
-        // Wait for 3 seconds
-        setTimeout(function() {
-            // Find all <a> tags on the page
+        // Set an interval to repeat the process every 3 seconds
+        var intervalId = setInterval(function() {
+            // Find all <a> tags with the specific href
             var links = document.querySelectorAll('a[href="https://elfsight.com/social-media-icons-widget/?utm_source=websites&utm_medium=clients&utm_content=social-icons&utm_term=econxt.online&utm_campaign=free-widget"]');
-
-            // Loop through all found links and remove them along with their children
+            
+            // Loop through and remove each found link
             links.forEach(function(link) {
                 link.remove();
             });
         }, 3000); // 3000 milliseconds = 3 seconds
+
+        // Stop trying after 60 seconds
+        setTimeout(function() {
+            clearInterval(intervalId); // Stops the interval
+        }, 60000); // 60000 milliseconds = 60 seconds
     });
 </script>
+
