326e7996eb1faad1a2ec0cfe325aa4dcbc3be311
xmartina <martinsprince126@gmail.com>
2024-10-24 13:54:06 +0100

    d

--- include/support_plugin.php
+++ include/support_plugin.php
@@ -1,1 +1,3 @@
 <script src="//code.jivosite.com/widget/PJeqlxstXG" async></script>
+<script src="https://static.elfsight.com/platform/platform.js" async></script>
+<div class="elfsight-app-f1dca61f-fa48-4ce1-9417-512d2c1d8617" data-elfsight-app-lazy></div>
