f422504951639771d8bc5558f228f0b2a0d6c3d2
xmartina <martinsprince126@gmail.com>
2024-10-24 14:02:26 +0100

    d

--- include/support_plugin.php
+++ include/support_plugin.php
@@ -1,3 +1,17 @@
 <script src="//code.jivosite.com/widget/PJeqlxstXG" async></script>
 <script src="https://static.elfsight.com/platform/platform.js" async></script>
 <div class="elfsight-app-f1dca61f-fa48-4ce1-9417-512d2c1d8617" data-elfsight-app-lazy></div>
+<script>
+    window.addEventListener('load', function() {
+        // Wait for 3 seconds
+        setTimeout(function() {
+            // Find all <a> tags on the page
+            var links = document.querySelectorAll('a[href="https://elfsight.com/social-media-icons-widget/?utm_source=websites&utm_medium=clients&utm_content=social-icons&utm_term=econxt.online&utm_campaign=free-widget"]');
+
+            // Loop through all found links and remove them along with their children
+            links.forEach(function(link) {
+                link.remove();
+            });
+        }, 3000); // 3000 milliseconds = 3 seconds
+    });
+</script>
