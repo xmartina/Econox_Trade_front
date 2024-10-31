26c252dbf7eab1ac49f4768edb6e81262971d2e4
xmartina <martinsprince126@gmail.com>
2024-10-31 09:03:34 +0100

    d

--- include/db_connect.php
+++ include/db_connect.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection parameters
 $servername = "localhost";  // Database server
-$username = "multistream6_econxt_back";         // Database username
-$password = "econxt_back";             // Database password
-$dbname = "multistream6_econxt_back";        // Database name
+$username = "multistream6_econoxtrades_back";         // Database username
+$password = "econoxtrades_back";             // Database password
+$dbname = "multistream6_econoxtrades_back";        // Database name
 
 // Create a connection
 $conn = new mysqli($servername, $username, $password, $dbname);
